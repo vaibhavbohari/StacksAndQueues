9e175ff356846007ee05bd3d88f363c4f42b8910
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-29 19:02:49 +0530

    Create a Queue

---
+++ StacksAndQueues/StacksAndQueues/LinkedListQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    public class LinkedListQueue
+    {
+        Node head = null;
+
+        /// <summary>
+        /// Method to insert an element to the end of the queue
+        /// </summary>
+        /// <param name="data"></param>
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data + " inserted into queue");
+        }
+
+        /// <summary>
+        /// Method to display queue
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is Empty!");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+   
+
--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -10,14 +10,25 @@ namespace StacksAndQueues
     {
         static void Main(string[] args)
         {
-            // Creating object
-            LinkedListStack my_Stack = new LinkedListStack();
-            my_Stack.Push(70);
-            my_Stack.Push(30);
-            my_Stack.Push(56);
-            my_Stack.Display();
-            my_Stack.IsEmpty();
-            my_Stack.Display();
+            //// Creating object
+            //LinkedListStack my_Stack = new LinkedListStack();
+            //my_Stack.Push(70);
+            //my_Stack.Push(30);
+            //my_Stack.Push(56);
+            //my_Stack.Display();
+            //my_Stack.IsEmpty();
+    
+
+
+
+
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
         }
     }
 }
+    
+  
